dcf1c5df08257de4b3252bf736a75aa55b8a6c81
清羽 <36099291+w2r@users.noreply.github.com>
2023-07-06 20:02:30 +0800

    Update post2tg.py

--- post2tg.py
+++ post2tg.py
@@ -4,6 +4,14 @@ import requests
 
 telegram_token = os.environ["telegram_token"]
 user_id = os.environ["user_id"]
+cdn_name = os.environ["cdn"]
+cdn = {
+    "jsdelivr": "https://cdn.jsdelivr.net/npm/",
+    "zhimg": "https://unpkg.zhimg.com/",
+    "bdstatic": "https://code.bdstatic.com/npm/",
+    "eleme": "https://shadow.elemecdn.com/npm/",
+    "unpkg": "https://unpkg.com/",
+}
 # 获得publish_id
 with open("package.json", "r") as f:
     publish_version = json.load(f)["version"]
@@ -12,4 +20,4 @@ with open("package.json", "r") as f:
 for path, dirs, files in os.walk("rawimg/"):
     for f in files:
         if ".webp" in f:
-            requests.get(f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={user_id}&text=" + f"https://cdn.jsdelivr.net/npm/{publish_version}@{publish_version}/" + os.path.join(path, f).replace("\\", "/"))
+            requests.get(f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={user_id}&text=" + f"{cdn['cdn_name']}{publish_version}@{publish_version}/" + os.path.join(path, f).replace("\\", "/"))
