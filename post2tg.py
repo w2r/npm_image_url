a2e9fb9bc1a87c6acd69bfd9007538a4f871a8e5
清羽 <36099291+w2r@users.noreply.github.com>
2023-07-06 15:20:33 +0800

    Update post2tg.py

--- post2tg.py
+++ post2tg.py
@@ -7,9 +7,10 @@ user_id = [2]
 
 # 获得publish_id
 with open("package.json", "r") as f:
-    publish_id = f.readlines()[3][12:17]
+    publish_id = f.readlines()[3][18:23]
     print(publish_id)
 # 推送到telegram
 for path, dirs, files in os.walk("rawimg/"):
     for f in files:
+        print(f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={user_id}&text=" + "https://cdn.jsdelivr.net/npm/w2r@{publish_id}/" + os.path.join(path, f).replace("\\", "/"))
         requests.get(f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={user_id}&text=" + "https://cdn.jsdelivr.net/npm/w2r@{publish_id}/" + os.path.join(path, f).replace("\\", "/"))
